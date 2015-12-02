ef8b7ee2422516c2d0c6518c6d808a4d7de56e8b
Diederik Krols <diederik_krols@hotmail.com>
2015-12-02 21:11:59 +0100

    Mondrianic distribution of gauges.

--- XamlBrewer.Uwp.RadialGaugeSample/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.RadialGaugeSample/ViewModels/ShellViewModel.cs
@@ -1,5 +1,7 @@
 ﻿// using XamlBrewer.UWP.MoonSharpSample.Views;
 
+using XamlBrewer.Uwp.RadialGaugeSample;
+
 namespace Mvvm
 {
     class ShellViewModel : ViewModelBase
@@ -7,7 +9,7 @@ namespace Mvvm
         public ShellViewModel()
         {
             // Build the menu
-            //Menu.Add(new MenuItem() { Glyph = "\uE1D0", Text = "Expressions", NavigationDestination = typeof(ExpressionPage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE2B1", Text = "Styles", NavigationDestination = typeof(MainPage) });
             //Menu.Add(new MenuItem() { Glyph = "\uE2B3", Text = "Lua Functions", NavigationDestination = typeof(LuaFunctionPage) });
             //Menu.Add(new MenuItem() { Glyph = "\uE2B4", Text = "C# Functions", NavigationDestination = typeof(CSharpFunctionPage) });
             //Menu.Add(new MenuItem() { Glyph = "\uE1CA", Text = "Object Sharing", NavigationDestination = typeof(ObjectSharingPage) });
